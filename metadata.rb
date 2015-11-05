11722a365fcc9615fe5f1a4f8ba2c9e882ceccaa
Christoph Lukas <lukas@pingworks.de>
2015-11-05 14:47:49 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'christoph.lukas@gmx.net'
 license          'All rights reserved'
 description      'Installs/Configures jenkins'
 long_description 'Installs/Configures jenkins'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'apt'
 depends 'ark'
